5740c60bc25529f679880750462d93fcbbbed7d7
Hussam <Hussam@DESKTOP-5ASCBET>
2019-04-20 14:05:50 -0400

    my second java release

---
+++ src/SITS/new_code.java
@@ -0,0 +1,13 @@
+package SITS;
+
+public class new_code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Helly everyone");
+		
+
+	}
+
+}
